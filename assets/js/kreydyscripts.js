309ca4cf15f6e541230511c3493ccae20fe24552
mxbialostocki <mxbialostocki@users.noreply.github.com>
2018-10-14 18:21:57 +1300

    discussed this ed with priestess of outerchurch, committing to this as base version with hope for develop in future once js skills allow for more carefully structured audio components 1810141820

--- assets/js/kreydyscripts.js
+++ assets/js/kreydyscripts.js
@@ -36,3 +36,56 @@ audio.addEventListener("load", function() {
 audio.src = source;
 audio.autoplay = true; // add this
 */
+
+
+
+/*
+$(document).ready(function() {
+    var audioElement = document.createElement('audio');
+    audioElement.setAttribute('src', 'http://www.soundjay.com/misc/sounds/bell-ringing-01.mp3');
+
+    audioElement.addEventListener('ended', function() {
+        this.play();
+    }, false);
+
+    audioElement.addEventListener("canplay",function(){
+        $("#length").text("Duration:" + audioElement.duration + " seconds");
+        $("#source").text("Source:" + audioElement.src);
+        $("#status").text("Status: Ready to play").css("color","green");
+    });
+
+    audioElement.addEventListener("timeupdate",function(){
+        $("#currentTime").text("Current second:" + audioElement.currentTime);
+    });
+
+    $('#play').click(function() {
+        audioElement.play();
+        $("#status").text("Status: Playing");
+    });
+
+    $('#pause').click(function() {
+        audioElement.pause();
+        $("#status").text("Status: Paused");
+    });
+
+    $('#restart').click(function() {
+        audioElement.currentTime = 0;
+    });
+});
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+<body>
+    <h2>Sound Information</h2>
+    <div id="length">Duration:</div>
+    <div id="source">Source:</div>
+    <div id="status" style="color:red;">Status: Loading</div>
+    <hr>
+    <h2>Control Buttons</h2>
+    <button id="play">Play</button>
+    <button id="pause">Pause</button>
+    <button id="restart">Restart</button>
+    <hr>
+    <h2>Playing Information</h2>
+    <div id="currentTime">0</div>
+</body>
+*/
