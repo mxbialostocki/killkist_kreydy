be0c96823915b7142b14cf499f756947ab7fdd9e
mxbialostocki <mxbialostocki@users.noreply.github.com>
2018-10-14 14:06:04 +1300

    throbs finalised, kk:hover finalised, main sound still not autoplaying. Visual dissonance preventing access to text body. still not sure how to allow :hover without document engagement 1810141406

--- assets/js/kreydyscripts.js
+++ assets/js/kreydyscripts.js
@@ -1,11 +1,38 @@
-
+// this section edited from https://stackoverflow.com/questions/15010909/using-jquery-to-play-audio-on-hover#15011106
 $( window ).on( "load", function(){
-    $("span#kreydyhov").hover(function(){
+    $("span.kreydyhov").hover(function(){
         kinstance.play();
         },
     function(){
         kinstance.load();
     });
 });
+/*
+var audioElement = document.createElement('audio');
+audioElement.setAttribute('src', 'YourSong.ogg');
+audioElement.load()
+audioElement.addEventListener("load", function() {
+  audioElement.play();
+}, true);
+*/
 
-// edited from https://stackoverflow.com/questions/15010909/using-jquery-to-play-audio-on-hover#15011106
+var source = "././assets/killkist_kreydy_kreyyd.mp3";
+ var audio = document.createElement("audio");
+ //
+ audio.autoplay = true;
+ //
+ audio.load()
+ audio.addEventListener("load", function() {
+     audio.play();
+ }, true);
+ audio.src = source;
+
+/*
+var source = "./assets/killkist_kreydy_kreyyd.mp3";
+var audio = new Audio(); // use the constructor in JavaScript, just easier that way
+audio.addEventListener("load", function() {
+  audio.play();
+}, true);
+audio.src = source;
+audio.autoplay = true; // add this
+*/
